74bc74c176e34c3440e3a3b18e76fc556ef77d0b
dxc0203 <51397967+dxc0203@users.noreply.github.com>
2026-03-12 21:17:08 -0400

    Create directories for model and scene promptsSeparate model and scene prompt directories
    Added directories for model and scene prompts.

--- src/app/constants.py
+++ src/app/constants.py
@@ -5,4 +5,9 @@ LOGS_DIR = os.path.join(ROOT_DIR, "logs")
 PROMPTS_DIR = os.path.join(ROOT_DIR, "Prompts")
 os.makedirs(PROMPTS_DIR, exist_ok=True)
 
+MODEL_PROMPTS_DIR = os.path.join(PROMPTS_DIR, "Models")
+SCENE_PROMPTS_DIR = os.path.join(PROMPTS_DIR, "Scenes")
+os.makedirs(MODEL_PROMPTS_DIR, exist_ok=True)
+os.makedirs(SCENE_PROMPTS_DIR, exist_ok=True)
+
 LANG = "zh-CN"
